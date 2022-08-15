9fd2338df2b797913171f284880d4081632b3bb3
MeherHassan <mchowdh8@lakeheadu.ca>
2022-08-15 01:12:44 -0400

    v1

---
+++ app.js
@@ -0,0 +1,69 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+var items = ["Buy", "Cook", "Eat"];
+var workItems = [];
+app.set("view engine", 'ejs');
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(express.static("public"));
+
+
+
+app.get("/", function(req, res) {
+  var today = new Date();
+  var currentDay = today.getDay();
+
+  var options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+  };
+
+  var day = today.toLocaleDateString("en-US", options);
+
+  res.render("list", {
+    kindofday: day,
+    listitem: items
+  });
+});
+
+app.post("/", function(req, res) {
+  var item = req.body.list;
+  if (req.body.button === "work") {
+    workItems.push("item");
+    res.redirect("/work");
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
+
+});
+
+
+app.get("/work", function(req, res) {
+  res.render("list", {
+    kindofday: "Work List",
+    listitem: workItems
+  });
+})
+
+// app.post("/work",function(req,res){
+//   let item = req.body.list;
+//   workItems.push(item);
+//   res.redirect("/work");
+// })
+
+
+
+
+
+
+
+app.listen(3000, function() {
+  console.log("3000 running server");
+})
